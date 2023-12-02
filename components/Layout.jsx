93487649cdda0a6e2429ed29296db16ad1e73edd
naledi-dikgale <naledi22khosi@gmail.com>
2023-12-02 11:26:53 +0200

    Update Styling

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,32 @@
+// import Head from 'next/head';
+// import { Box } from '@chakra-ui/react';
+
+// import Footer from './Footer';
+// import Navbar from './Navbar';
+
+// export default function Layout({ children }) {
+//   return (
+//     <>
+//       <Head>
+//         <title>Home Finder</title>
+//       </Head>
+//       <Box maxWidth='1280px' m='auto'
+//        bgGradient='linear(to-r, teal.50, blue.100)'
+//       //  h='100vh'
+//       >
+//         <header>
+//           <Navbar />
+//         </header>
+//         <main>{children}</main>
+//         <footer>
+//           <Footer />
+//         </footer>
+//       </Box>
+//     </>
+//   );
+// }
+
 import Head from 'next/head';
-import { Box } from '@chakra-ui/react';
 
 import Footer from './Footer';
 import Navbar from './Navbar';
@@ -10,10 +37,7 @@ export default function Layout({ children }) {
       <Head>
         <title>Home Finder</title>
       </Head>
-      <Box maxWidth='1280px' m='auto'
-       bgGradient='linear(to-r, teal.50, blue.100)'
-      //  h='100vh'
-      >
+      <div>
         <header>
           <Navbar />
         </header>
@@ -21,7 +45,7 @@ export default function Layout({ children }) {
         <footer>
           <Footer />
         </footer>
-      </Box>
+      </div>
     </>
   );
 }

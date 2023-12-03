2ca3bae3cdd32bb8d76da511885cd8144a0a6d33
naledi-dikgale <naledi22khosi@gmail.com>
2023-12-03 21:31:06 +0200

    Fix bugs

--- pages/_app.js
+++ pages/_app.js
@@ -32,7 +32,7 @@
 
 // export default MyApp;
 
-import './styles.css';
+import './global.css';
 import Router from 'next/router';
 import Head from 'next/head';
 import NProgress from 'nprogress';
@@ -53,7 +53,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
+        {/* <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' /> */}
       </Head>
       <Layout>
         <Component {...pageProps} />

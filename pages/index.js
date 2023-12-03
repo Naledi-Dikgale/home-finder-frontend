5f61eb62031c616c3d2a1babbdadd529f36cae02
naledi-dikgale <naledi22khosi@gmail.com>
2023-12-03 21:31:25 +0200

    Fix bugs

--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,13 @@
-// import Link from 'next/link';
-// import Image from 'next/image';
-// import { Flex, Box, Text, Button } from '@chakra-ui/react';
-
-// import Property from '../components/Property';
-// import { baseUrl, fetchApi } from '../utils/fetchApi';
-
-// export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
-//   <Flex flexWrap='wrap' justifyContent='start' m='10' borderRadius='8px' overflow='hidden'>
-//     <Image src={imageUrl} width={400} height={200} />
-//     <Box p='5'>
-//       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
-//       <Text fontSize='3xl' fontWeight='bold' color='blue.900'>{title1}<br />{title2}</Text>
-//       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='blue.900'>{desc1}<br />{desc2}</Text>
-//       <Button fontSize='xl' bg="pink.300" color="white">
-//         <Link href={linkName}>{buttonText}</Link>
-//       </Button>
-//     </Box>
-//   </Flex>
-// );
-
-
-// const Home = ({ propertiesForSale, propertiesForRent }) => (
-//   <Box>
-//     <Banner
-//       purpose='RENT A HOME'
-//       title1='Rental Homes for'
-//       title2='All'
-//       desc1=' Browse through Apartments, houses'
-//       desc2='and more'
-//       buttonText='See Options'
-//       linkName='/search?purpose=for-rent'
-//       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
-//     />
-   
-//     <Banner
-//       purpose='BUY A HOME'
-//       title1=' Find & Buy Your'
-//       title2='Dream Home'
-//       desc1=' Browse from Apartments, houses,'
-//       desc2=' and more'
-//       buttonText='See Options'
-//       linkName='/search?purpose=for-sale'
-//       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
-//     />
-    
-//   </Box>
-// );
-
-// export async function getStaticProps() {
-//   const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
-//   const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
-
-//   return {
-//     props: {
-//       propertiesForSale: propertyForSale?.hits,
-//       propertiesForRent: propertyForRent?.hits,
-//     },
-//   };
-// }
-
-// export default Home;
 
 import Link from 'next/link';
-// import Image from 'next/image';
 
 import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
-export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
+export const Banner = ({ buttonText, linkName }) => (
   <div>
-    {/* <Image src={imageUrl} width={400} height={200} /> */}
-    <div>
-      <p>{purpose}</p>
-      <p>{title1}<br />{title2}</p>
-      <p>{desc1}<br />{desc2}</p>
-      <button>
+    <div className="bg-white">
+      <button className=" text-black px-4 py-2 rounded hover:bg-green-400 hover:text-white transition duration-300 ease-in-out">
         <Link href={linkName}>{buttonText}</Link>
       </button>
     </div>
@@ -82,28 +15,14 @@ export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, link
 );
 
 const Home = ({ propertiesForSale, propertiesForRent }) => (
-  <div>
-    <Banner
-      // purpose='RENTALS'
-      // title1='Rental Homes for'
-      // title2='All'
-      // desc1=' Browse through Apartments, houses'
-      // desc2='and more'
-      buttonText='RENTALS'
-      linkName='/search?purpose=for-rent'
-      // imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
-    />
-   
-    <Banner
-      // purpose='SALES'
-      // title1=' Find & Buy Your'
-      // title2='Dream Home'
-      // desc1=' Browse from Apartments, houses,'
-      // desc2='and more'
-      buttonText='SALES'
-      linkName='/search?purpose=for-sale'
-      // imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
-    />
+  <div className="h-screen flex flex-col">
+    <div className="h-1/2 bg-black text-green-400 flex justify-center">
+      <Banner buttonText='RENTALS' linkName='/search?purpose=for-rent' />
+      <Banner buttonText='SALES' linkName='/search?purpose=for-sale' />
+    </div>
+
+    <div className="h-1/2 bg-white">
+    </div>
   </div>
 );
 
